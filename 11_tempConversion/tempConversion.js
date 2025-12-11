acb887643ed95383cd061c57aabe8e619eb1384f
Sai-Eshwar-Supreet <saieshwarsupreet@gmail.com>
2025-12-11 06:27:26 +0000

    Complete 11_tempConversion exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,11 @@
-const convertToCelsius = function() {
-};
+const roundTo = (number, precision) => {
+  let factor = 10**precision;
+  return Math.round(number * factor) / factor;
+}
 
-const convertToFahrenheit = function() {
-};
+const convertToCelsius = (f)  => roundTo((f - 32) * 5/9, 1);
+
+const convertToFahrenheit = (c) =>  roundTo((c * 9/5) + 32 , 1);
 
 // Do not edit below this line
 module.exports = {
