99f519c9c0553044d994cf73d197ca550e39a4f0
Sai-Eshwar-Supreet <saieshwarsupreet@gmail.com>
2025-12-11 05:56:27 +0000

    Complete 10_leapYears exercise

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    let isDivisibleByFour = year % 4 === 0;
+    let isDivisibleByHundred = year % 100 === 0;
+    let isDivisibleByFourHundred = year % 400 === 0;
+
+    return isDivisibleByFourHundred || (!isDivisibleByHundred && isDivisibleByFour);
 };
 
 // Do not edit below this line
