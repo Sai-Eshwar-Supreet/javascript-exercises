ad43abccdfee664e2bc29c900b2d252ff3ce7e06
Sai-Eshwar-Supreet <saieshwarsupreet@gmail.com>
2025-12-11 05:48:05 +0000

    Complete 09_sumAll exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) return "ERROR";
 
+    if(a > b) [a ,b] = [b,a];
+
+    let result = 0; 
+
+    for(let i = a; i <= b; i++) result += i;
+
+    return result;
 };
 
 // Do not edit below this line
