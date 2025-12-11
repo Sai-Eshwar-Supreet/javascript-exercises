9010de638765e39c220554841296b8ca81ce6cdc
Sai-Eshwar-Supreet <saieshwarsupreet@gmail.com>
2025-12-11 05:16:12 +0000

    Complete 07_reverseString exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let result = new Array(str.length);
+    for(let i = 0 - 1; i < str.length; i++) result[i] = str[str.length - 1 - i];
 
+    return result.join("");
 };
 
 // Do not edit below this line
