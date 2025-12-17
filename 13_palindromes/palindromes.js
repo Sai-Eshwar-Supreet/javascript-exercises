d0b6ca15f526b656b1ae5bc4903dfc7f23017fa6
Sai-Eshwar-Supreet <saieshwarsupreet@gmail.com>
2025-12-17 04:35:30 +0000

    Complete 13_palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    var replaced = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    var reversed = [...replaced].reverse().join('');
+    return reversed === replaced;
 };
 
 // Do not edit below this line
