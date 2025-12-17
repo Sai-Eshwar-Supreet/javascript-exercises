b7351813d168f7e514a4ff490c0df865ff9be2c5
Sai-Eshwar-Supreet <saieshwarsupreet@gmail.com>
2025-12-17 03:52:17 +0000

    Complete 12_calculator exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,26 +1,14 @@
-const add = function() {
-	
-};
+const add = (a, b) => a + b;
 
-const subtract = function() {
-	
-};
+const subtract = (a, b) => a - b;
 
-const sum = function() {
-	
-};
+const sum = (arr) => arr.reduce( (total, item) => total + item, 0);
 
-const multiply = function() {
+const multiply = (arr) => arr.reduce( (total, item) => total * item, 1);
 
-};
+const power = (base, exponent) => base**exponent;
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = (num) => num <= 1? 1 : num * factorial(num - 1);
 
 // Do not edit below this line
 module.exports = {
