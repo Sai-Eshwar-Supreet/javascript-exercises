4d6ca30f9c77275a079101a71c66d3e5fbb21809
Sai-Eshwar-Supreet <saieshwarsupreet@gmail.com>
2025-12-17 07:58:30 +0000

    Complete 16_findTheOldest exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const getValidYOD = (yod) => (yod ?? (new Date()).getFullYear());
+    let oldest = people.reduce((oldest, person) =>{
+        let oldestAge = getValidYOD(oldest.yearOfDeath) - oldest.yearOfBirth;
+        let personAge = getValidYOD(person.yearOfDeath) - person.yearOfBirth;
 
+        return oldestAge > personAge ? oldest : person;
+    });
+
+    return oldest;
 };
 
 // Do not edit below this line
