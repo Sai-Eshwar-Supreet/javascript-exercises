82896c9af73f96bd2c3db058c9eb4744b60a4a75
Sai-Eshwar-Supreet <saieshwarsupreet@gmail.com>
2025-12-17 07:31:50 +0000

    Complete 14_fibonacci exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if(n < 0) return "OOPS";
+    let previous = 1;
+    let result = 0;
+
+    for(let i = 0; i < n; i++){
+        let current = result;
+        result = result + previous;
+        previous = current;
+    }
+
+    return result;
 
 };
 
