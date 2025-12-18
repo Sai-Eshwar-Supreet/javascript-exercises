1d3bb2022312663debbcd3e8e46028ecd8ad7da3
Sai-Eshwar-Supreet <saieshwarsupreet@gmail.com>
2025-12-18 15:20:51 +0000

    Complete 20_permutations exercise

--- 20_permutations/permutations.js
+++ 20_permutations/permutations.js
@@ -1,6 +1,19 @@
-const permutations = function() {
-  
-};
+const permutations = (arr) =>{
+
+    if(arr.length <= 1) return [arr];
+    
+    let result = [];
+
+    for(let i = 0; i < arr.length; i++){
+        let val = arr[i];
+        let newArr = [...arr.slice(0, i), ...arr.slice(i+1, arr.length)];
+        for(let item of permutations(newArr)){
+            result.push([val, ...item]);
+        }
+    }
+
+    return result;
+}
   
 // Do not edit below this line
 module.exports = permutations;
