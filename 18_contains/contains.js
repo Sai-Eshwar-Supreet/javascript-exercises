c14c01c25e7d6c4bf216f3478248336aea94abe6
Sai-Eshwar-Supreet <saieshwarsupreet@gmail.com>
2025-12-18 13:36:36 +0000

    Complete 18_contains exercise

--- 18_contains/contains.js
+++ 18_contains/contains.js
@@ -1,5 +1,10 @@
-const contains = function() {
-  
+const contains = function(obj, target) {
+    for(const key in obj){
+        const val = obj[key];
+        if(Object.is(val, target)) return true;
+        if(val !== null && typeof(val) === 'object' && contains(val, target)) return true;
+    }
+    return false;
 };
   
 // Do not edit below this line
