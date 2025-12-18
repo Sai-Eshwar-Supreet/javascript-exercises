f963b07541d1c064b9a9d7e03352875f1d28307c
Sai-Eshwar-Supreet <saieshwarsupreet@gmail.com>
2025-12-18 13:52:56 +0000

    Complete 19_totalIntegers exercise

--- 19_totalIntegers/totalIntegers.js
+++ 19_totalIntegers/totalIntegers.js
@@ -1,5 +1,15 @@
-const totalIntegers = function() {
-  
+const isObject = (val) => val !== null && typeof(val) === 'object';
+const totalIntegers = function(obj) {
+    if(!isObject(obj)) return;
+    
+    let count = 0;
+    for(const key in obj){
+        const val = obj[key];
+        if(Number.isInteger(val)) count++;
+        else count += (totalIntegers(val) ?? 0);
+    }
+
+    return count;
 };
   
 // Do not edit below this line
