d12d231714927025b9c60ff132cf6c77e02a654a
Sai-Eshwar-Supreet <saieshwarsupreet@gmail.com>
2025-12-19 18:14:42 +0000

    Complete 21_pascal exercise

--- 21_pascal/pascal.js
+++ 21_pascal/pascal.js
@@ -1,5 +1,17 @@
-const pascal = function() {
-  
+const pascal = function(n) {
+
+    if(n <= 0) return [];
+
+    let currentRow = [1];
+    if(n === 1) return currentRow;
+
+
+    const previousRow = pascal(n - 1);
+    for(let i = 0; i <= previousRow.length; i++){
+        currentRow[i] = (previousRow[i] ?? 0) + (previousRow[i - 1] ?? 0);
+    }
+
+    return currentRow;
 };
   
 // Do not edit below this line
